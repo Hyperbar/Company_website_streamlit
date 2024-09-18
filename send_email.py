63379e8909a814dad499f0af0056824aa4f52f0f
hyperbar <szthyperbar@gmail.com>
2024-09-18 15:38:10 +0200

    Secure password

--- send_email.py
+++ send_email.py
@@ -1,13 +1,14 @@
 import smtplib, ssl
-
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
-    username = ""
-    password = "wema wpqr ngkt coxb"
-    receiver = ""
+    username = "szthyperbar@gmail.com"
+    password = os.getenv("PASSWORD")
+    receiver = "samuelsztabholz@gmail.com"
     my_context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=my_context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
+
